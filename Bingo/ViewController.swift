182b33283ca32c4803b3a01318ca76e6cef979ea
Arne Bahlo <hallo@arne.me>
2015-09-18 10:25:08 +0200

    Initial Commit

---
+++ Bingo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bingo
+//
+//  Created by Arne Bahlo on 18/09/15.
+//  Copyright © 2015 Arne Bahlo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
